f43742fa301e355e1c54462737b96d30ee54fe44
Antoine Lavandier <antoine.lavandier@ecl19.ec-lyon.fr>
2022-08-11 21:04:03 +0200

    Reformat with black isort and flake8

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,28 +1,22 @@
-import pytest
 import os
 
 import numpy as np
-
+import pytest
+from utils_test import get_vtk_data
 from vtk import (
-    vtkXMLRectilinearGridReader,
     vtkXMLImageDataReader,
+    vtkXMLRectilinearGridReader,
     vtkXMLStructuredGridReader,
     vtkXMLUnstructuredGridReader,
 )
 
-from utils_test import get_vtk_data
-
-from py2vtk.core.vtkfiles import (VtkFile,
-                                  VtkImageData,
-                                  VtkRectilinearGrid,
-                                  VtkStructuredGrid,
-                                  VtkUnstructuredGrid)
-
-from py2vtk.core.vtkcells import (
-    VtkTriangle,
-    VtkVertex,
-    VtkLine,
-    VtkPolyLine,
+from py2vtk.core.vtkcells import VtkLine, VtkPolyLine, VtkTriangle, VtkVertex
+from py2vtk.core.vtkfiles import (
+    VtkFile,
+    VtkImageData,
+    VtkRectilinearGrid,
+    VtkStructuredGrid,
+    VtkUnstructuredGrid,
 )
 
 # Tolerance for float equality
@@ -30,20 +24,20 @@ ATOL = 1e-15
 RTOL = 1e-15
 
 
-@pytest.mark.parametrize('compressor', ['zlib', 'lzma'])
-@pytest.mark.parametrize('compression', [True, False, 5])
-@pytest.mark.parametrize('direct_format', ['binary', 'ascii'])
-@pytest.mark.parametrize('appended_format', ['binary', 'raw'])
+@pytest.mark.parametrize("compressor", ["zlib", "lzma"])
+@pytest.mark.parametrize("compression", [True, False, 5])
+@pytest.mark.parametrize("direct_format", ["binary", "ascii"])
+@pytest.mark.parametrize("appended_format", ["binary", "raw"])
 def test_image_data(compressor, compression, direct_format, appended_format):
     origin = 0, 3, 6
     spacing = 1.0, 2.0, 4.0
 
-    filepath = 'image_data'
+    filepath = "image_data"
 
     pointdata = np.random.random((6, 11, 8))
     celldata = np.random.random((5, 10, 7))
 
-    if not compression is False and appended_format == "raw":
+    if compression is not False and appended_format == "raw":
         with pytest.warns(UserWarning):
             vtk_file = VtkFile(
                 filepath=filepath,
@@ -63,12 +57,10 @@ def test_image_data(compressor, compression, direct_format, appended_format):
             compressor=compressor,
         )
 
-    vtk_file.openGrid(
-        start=(0, 0, 0), end=(5, 10, 7), origin=origin, spacing=spacing
-    )
+    vtk_file.openGrid(start=(0, 0, 0), end=(5, 10, 7), origin=origin, spacing=spacing)
     vtk_file.openPiece(start=(0, 0, 0), end=(5, 10, 7))
 
-    vtk_file.openData("Point", scalars='points')
+    vtk_file.openData("Point", scalars="points")
     vtk_file.addData("points", pointdata, append=True)
     vtk_file.closeData("Point")
 
@@ -89,8 +81,8 @@ def test_image_data(compressor, compression, direct_format, appended_format):
 
     vtk_point, vtk_cell, vtk_field = get_vtk_data(reader, vtk_file.getFileName())
 
-    vtk_point = vtk_point.reshape(pointdata.shape, order='F')
-    vtk_cell = vtk_cell.reshape(celldata.shape, order='F')
+    vtk_point = vtk_point.reshape(pointdata.shape, order="F")
+    vtk_cell = vtk_cell.reshape(celldata.shape, order="F")
 
     assert vtk_field == [0]
     assert np.allclose(vtk_point, pointdata, atol=ATOL, rtol=RTOL)
@@ -99,12 +91,12 @@ def test_image_data(compressor, compression, direct_format, appended_format):
     os.remove(vtk_file.getFileName())
 
 
-@pytest.mark.parametrize('compressor', ['zlib', 'lzma'])
-@pytest.mark.parametrize('compression', [True, False, 5])
-@pytest.mark.parametrize('direct_format', ['binary', 'ascii'])
-@pytest.mark.parametrize('appended_format', ['binary', 'raw'])
+@pytest.mark.parametrize("compressor", ["zlib", "lzma"])
+@pytest.mark.parametrize("compression", [True, False, 5])
+@pytest.mark.parametrize("direct_format", ["binary", "ascii"])
+@pytest.mark.parametrize("appended_format", ["binary", "raw"])
 def test_rectilinear_grid(compressor, compression, direct_format, appended_format):
-    filepath = 'rectilinear_grid'
+    filepath = "rectilinear_grid"
 
     x = np.random.random(6)
     y = np.random.random(11)
@@ -112,7 +104,7 @@ def test_rectilinear_grid(compressor, compression, direct_format, appended_forma
     pointdata = np.random.random((6, 11, 8))
     celldata = np.random.random((5, 10, 7))
 
-    if not compression is False and appended_format == "raw":
+    if compression is not False and appended_format == "raw":
         with pytest.warns(UserWarning):
             vtk_file = VtkFile(
                 filepath=filepath,
@@ -132,12 +124,10 @@ def test_rectilinear_grid(compressor, compression, direct_format, appended_forma
             compressor=compressor,
         )
 
-    vtk_file.openGrid(
-        start=(0, 0, 0), end=(5, 10, 7)
-    )
+    vtk_file.openGrid(start=(0, 0, 0), end=(5, 10, 7))
     vtk_file.openPiece(start=(0, 0, 0), end=(5, 10, 7))
 
-    vtk_file.openData("Point", scalars='points')
+    vtk_file.openData("Point", scalars="points")
     vtk_file.addData("points", pointdata, append=True)
     vtk_file.closeData("Point")
 
@@ -164,8 +154,8 @@ def test_rectilinear_grid(compressor, compression, direct_format, appended_forma
 
     vtk_point, vtk_cell, vtk_field = get_vtk_data(reader, vtk_file.getFileName())
 
-    vtk_point = vtk_point.reshape(pointdata.shape, order='F')
-    vtk_cell = vtk_cell.reshape(celldata.shape, order='F')
+    vtk_point = vtk_point.reshape(pointdata.shape, order="F")
+    vtk_cell = vtk_cell.reshape(celldata.shape, order="F")
 
     assert vtk_field == [0]
     assert np.allclose(vtk_point, pointdata, atol=ATOL, rtol=RTOL)
@@ -174,12 +164,12 @@ def test_rectilinear_grid(compressor, compression, direct_format, appended_forma
     os.remove(vtk_file.getFileName())
 
 
-@pytest.mark.parametrize('compressor', ['zlib', 'lzma'])
-@pytest.mark.parametrize('compression', [True, False, 5])
-@pytest.mark.parametrize('direct_format', ['binary', 'ascii'])
-@pytest.mark.parametrize('appended_format', ['binary', 'raw'])
+@pytest.mark.parametrize("compressor", ["zlib", "lzma"])
+@pytest.mark.parametrize("compression", [True, False, 5])
+@pytest.mark.parametrize("direct_format", ["binary", "ascii"])
+@pytest.mark.parametrize("appended_format", ["binary", "raw"])
 def test_structured_grid(compressor, compression, direct_format, appended_format):
-    filepath = 'structured_grid'
+    filepath = "structured_grid"
 
     x = np.random.random((6, 11, 8))
     y = np.random.random((6, 11, 8))
@@ -188,7 +178,7 @@ def test_structured_grid(compressor, compression, direct_format, appended_format
     pointdata = np.random.random((6, 11, 8))
     celldata = np.random.random((5, 10, 7))
 
-    if not compression is False and appended_format == "raw":
+    if compression is not False and appended_format == "raw":
         with pytest.warns(UserWarning):
             vtk_file = VtkFile(
                 filepath=filepath,
@@ -208,12 +198,10 @@ def test_structured_grid(compressor, compression, direct_format, appended_format
             compressor=compressor,
         )
 
-    vtk_file.openGrid(
-        start=(0, 0, 0), end=(5, 10, 7)
-    )
+    vtk_file.openGrid(start=(0, 0, 0), end=(5, 10, 7))
     vtk_file.openPiece(start=(0, 0, 0), end=(5, 10, 7))
 
-    vtk_file.openData("Point", scalars='points')
+    vtk_file.openData("Point", scalars="points")
     vtk_file.addData("points", pointdata, append=False)
     vtk_file.closeData("Point")
 
@@ -238,8 +226,8 @@ def test_structured_grid(compressor, compression, direct_format, appended_format
 
     vtk_point, vtk_cell, vtk_field = get_vtk_data(reader, vtk_file.getFileName())
 
-    vtk_point = vtk_point.reshape(pointdata.shape, order='F')
-    vtk_cell = vtk_cell.reshape(celldata.shape, order='F')
+    vtk_point = vtk_point.reshape(pointdata.shape, order="F")
+    vtk_cell = vtk_cell.reshape(celldata.shape, order="F")
 
     assert vtk_field == [0]
     assert np.allclose(vtk_point, pointdata, atol=ATOL, rtol=RTOL)
@@ -248,30 +236,24 @@ def test_structured_grid(compressor, compression, direct_format, appended_format
     os.remove(vtk_file.getFileName())
 
 
-@pytest.mark.parametrize('compressor', ['zlib', 'lzma'])
-@pytest.mark.parametrize('compression', [True, False, 5])
-@pytest.mark.parametrize('direct_format', ['binary', 'ascii'])
-@pytest.mark.parametrize('appended_format', ['binary', 'raw'])
+@pytest.mark.parametrize("compressor", ["zlib", "lzma"])
+@pytest.mark.parametrize("compression", [True, False, 5])
+@pytest.mark.parametrize("direct_format", ["binary", "ascii"])
+@pytest.mark.parametrize("appended_format", ["binary", "raw"])
 def test_unstructured_grid(compressor, compression, direct_format, appended_format):
-    filepath = 'unstructured_grid'
+    filepath = "unstructured_grid"
 
     x = np.array([0, 0, 0])
     y = np.array([0, 1, 2])
     z = np.array([0.5, 2, 3])
 
-    connectivity = np.array(
-        [0, 1, 2, 1, 2, 1, 0, 2, 1]
-    )
-    offsets = np.array(
-        [3, 5, 6, 9]
-    )
-    celltypes = np.array(
-        [VtkTriangle.tid, VtkLine.tid, VtkVertex.tid, VtkPolyLine.tid]
-    )
+    connectivity = np.array([0, 1, 2, 1, 2, 1, 0, 2, 1])
+    offsets = np.array([3, 5, 6, 9])
+    celltypes = np.array([VtkTriangle.tid, VtkLine.tid, VtkVertex.tid, VtkPolyLine.tid])
     pointdata = np.random.random(3)
     celldata = np.random.random(4)
 
-    if not compression is False and appended_format == "raw":
+    if compression is not False and appended_format == "raw":
         with pytest.warns(UserWarning):
             vtk_file = VtkFile(
                 filepath=filepath,
@@ -294,7 +276,7 @@ def test_unstructured_grid(compressor, compression, direct_format, appended_form
     vtk_file.openGrid()
     vtk_file.openPiece(ncells=4, npoints=3)
 
-    vtk_file.openData("Point", scalars='points')
+    vtk_file.openData("Point", scalars="points")
     vtk_file.addData("points", pointdata, append=False)
     vtk_file.closeData("Point")
 
@@ -325,8 +307,8 @@ def test_unstructured_grid(compressor, compression, direct_format, appended_form
 
     vtk_point, vtk_cell, vtk_field = get_vtk_data(reader, vtk_file.getFileName())
 
-    vtk_point = vtk_point.reshape(pointdata.shape, order='F')
-    vtk_cell = vtk_cell.reshape(celldata.shape, order='F')
+    vtk_point = vtk_point.reshape(pointdata.shape, order="F")
+    vtk_cell = vtk_cell.reshape(celldata.shape, order="F")
 
     assert vtk_field == [0]
     assert np.allclose(vtk_point, pointdata, atol=ATOL, rtol=RTOL)
