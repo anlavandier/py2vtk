5b70aa35f0d90280d338d251b35b7cdb4f6df512
Antoine Lavandier <antoine.lavandier@cea.fr>
2022-08-11 11:18:58 +0200

    remove trailing whitespaces

--- py2vtk/__init__.py
+++ py2vtk/__init__.py
@@ -1,15 +1,19 @@
 from api import *
 from core import *
 
-__all__ = ['imageToVTK',
-           'gridToVTK',
-           'pointsToVTK',
-           'linestoVTK',
-           'unstructuredGridToVTK',
-           'cylinderToVTK',
-           'writeParallelVTKImageData',
-           'writeParallelVTKGrid',
-           'writeParallelVTKPolyData',
-           'writeParallelVTKUnstructuredGrid']
+__all__ = [
+    "imageToVTK",
+    "gridToVTK",
+    "pointsToVTK",
+    "linestoVTK",
+    "unstructuredGridToVTK",
+    "cylinderToVTK",
+    "writeParallelVTKImageData",
+    "writeParallelVTKGrid",
+    "writeParallelVTKPolyData",
+    "writeParallelVTKUnstructuredGrid",
+]
+
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
